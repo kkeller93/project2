4e019bcb8174a0aa79288d1a24dca8d6cea639f1
KEVIN KELLER <keller_kevin@bah.com>
2017-02-28 09:52:39 -0500

    migrations

--- car_service/db/migrate/20170228142306_create_cars.rb
+++ car_service/db/migrate/20170228142306_create_cars.rb
@@ -1,7 +1,12 @@
 class CreateCars < ActiveRecord::Migration[5.0]
   def change
     create_table :cars do |t|
-
+      t.integer :year
+      t.string :make
+      t.string :model
+      t.integer :mileage
+      t.string :customer
+      t.string :img_url
       t.timestamps
     end
   end
--- car_service/db/migrate/20170228142318_create_problems.rb
+++ car_service/db/migrate/20170228142318_create_problems.rb
@@ -1,7 +1,8 @@
 class CreateProblems < ActiveRecord::Migration[5.0]
   def change
     create_table :problems do |t|
-
+      t.string :problem
+      t.string :img_url
       t.timestamps
     end
   end
